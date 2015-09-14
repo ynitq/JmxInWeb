30eed2a63a7dd3be0e7305cc3c186015bc4bd77c
liangwj72 <liangwj72@qq.com>
2015-09-14 20:00:40 +0800

    add

---
+++ src/main/java/com/kunmingCoder/jmxInWeb/AppConstants.java
@@ -0,0 +1,17 @@
+package com.kunmingCoder.jmxInWeb;
+
+/**
+ * <pre>
+ * 
+ * </pre>
+ * 
+ * @author 梁韦江
+ * 2015年9月11日
+ */
+public interface AppConstants {
+
+	public static String STATIC_RESOURCE_PREFIX = "com/kunmingCoder/jmxInWeb/files/statics/";
+
+	public static String TEMPLATE_PATH = "com/kunmingCoder/jmxInWeb/files/";
+
+}
